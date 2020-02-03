460f0244e7306e0ba2188fb1b6ec0d28a4717558
durhambn <durhambn@g.cofc.edu>
2020-02-02 20:09:40 -0500

    working basic code

--- app/src/main/java/com/introtoandroid/button_counter_durham/MainActivity.java
+++ app/src/main/java/com/introtoandroid/button_counter_durham/MainActivity.java
@@ -3,12 +3,66 @@ package com.introtoandroid.button_counter_durham;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static com.introtoandroid.button_counter_durham.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
+//declare buttons and text that will be used in view
+    Button countBut;
+    Button clearBut;
+    TextView count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+
+
+        //Log.d("1", "In create 1");
+
+        //initialize buttons and text with their ids in view
+        countBut = (Button) findViewById(R.id.countButton);
+        clearBut = (Button) findViewById(R.id.clearButton);
+        count = (TextView) findViewById(R.id.countText);
+
+        //Log.d("1", "In create 2");
+        //Log.d("1", "In create 3");
+
+        //click listener for count button
+        //this button increased the count by 1
+        countBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("2", "inside click listener for count");
+                //initialize a local temporary variable to grab what is currently in the textview
+                //what is currently in count is a char so convert to string adn then to int
+                int temp = Integer.parseInt(count.getText().toString().trim());
+                //Log.d("2", "inside" + count.getText().toString());
+                temp++;
+                //set the text to the value plus one but convert back to string first
+                count.setText((Integer.toString(temp)));
+            }
+        });
+
+        //click listener for clear button
+        //this button resets the text view to 0
+        clearBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //grabs string from resources to set count
+                count.setText(getResources().getString(R.string.zero));
+            }
+        });
     }
+
+
+
 }
